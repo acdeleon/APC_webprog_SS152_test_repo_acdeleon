70b65c6ff45a9595692a174b6338ae02a641c4ed
Aleo Ralph C. De Leon <deleonaleoralphcastro@gmail.com>
2016-11-22 11:15:52 +0800

    fix things

--- excercise5/index.php
+++ excercise5/index.php
@@ -1,83 +1,84 @@
-<?php
-include_once 'dbconfig.php';
-
-// delete condition
-if(isset($_GET['delete_id']))
-{
- $sql_query="DELETE FROM users WHERE user_id=".$_GET['delete_id'];
- mysqli_query($con,$sql_query);
- header("Location: $_SERVER[PHP_SELF]");
-}
-// delete condition
-?>
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>CRUD Operations With PHP and MySql - By Cleartuts</title>
-<link rel="stylesheet" href="style.css" type="text/css" />
-<script type="text/javascript">
-function edt_id(id)
-{
- if(confirm('Sure to edit ?'))
- {
-  window.location.href='edit_data.php?edit_id='+id;
- }
+<title>Webprog</title>
+<style>
+h1{color:yellow;}
+h2{color: yellow;}
+body{outline-style: dotted;outline-color: yellow;}
+.r{font-family: verdana}
+body {background-image: url("c.jpg");}
+#h{border-radius: 50%;}
+.1{border-radius: 20%;}
+
+
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: rgba(255,255,255,0.5);
+	position: fixed;
+    top: 0;
+    width: 100%;
+
 }
-function delete_id(id)
-{
- if(confirm('Sure to Delete ?'))
- {
-  window.location.href='index.php?delete_id='+id;
- }
+
+li {
+    float: left;
+	
 }
-</script>
+
+li a  {
+    display: block;
+    color: black;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+	border-right: 1px solid #111;
+}
+
+li:hover{
+ background-color: yellow;}
+
+
+
+</style>
+
 </head>
-<body>
-<li class="hm"><a href="mypage.html"; style=float:left;>Home</a></li>
-<li class="hm"><a href="more.html"; style=float:left;>More about me</a></li>
-<li class="hm"><a href="Register.php"; style=float:left; >Contact me</a></li>
-<center>
 
-<div id="header">
+<ul>
+<li><a href="mypage.html">Home</a></li>
+<li><a href="more.html">More about me</a></li>
+<li><a href="Register.php">Contact me</a></li>
+</ul>
 
-    <label><ul>
-</ul></a></label>
-   
-</div>
 
-<div id="body">
- <div id="content">
-    <table align="center">
-    <tr>
-    <th colspan="5"><a href="add_data.php">add data here.</a></th>
-    </tr>
-    <th>First Name</th>
-    <th>Last Name</th>
-    <th>City Name</th>
-    <th colspan="2">Operations</th>
-    </tr>
-    <?php
- $sql_query="SELECT * FROM users";
- $result_set=mysqli_query($con, $sql_query);
- while($row=mysqli_fetch_row($result_set))
- {
-  ?>
-        <tr>
-        <td><?php echo $row[1]; ?></td>
-        <td><?php echo $row[2]; ?></td>
-        <td><?php echo $row[3]; ?></td>
-  <td align="center"><a href="javascript:edt_id('<?php echo $row[0]; ?>')"><img src="b_edit.png" align="EDIT" /></a></td>
-        <td align="center"><a href="javascript:delete_id('<?php echo $row[0]; ?>')"><img src="b_drop.png" align="DELETE" /></a></td>
-        </tr>
-        <?php
- }
- ?>
-    </table>
-    </div>
-</div>
+<br>
+<h1 class="r" style="text-align:center;">About me<br><h2 style="font-family:verdana;"><center>Aleo</br></center></h2>
+<center><img id="h" src="ar.jpg" alt="My Pic" style="width:200px;height:200x;"></center>
 
-</center>
+<p class="r" style="text-align:center;font-size:140%"><b>My Name: <br>Aleo Ralph C. De Leon</br></p></b>
+<p class="r" style="text-align:center;font-size:140%"><b>My Hobbies and Interests: <br>Computer Games, Movies, Badminton, Table Tennis, Basketball</br></p></b>
+
+<p class = "r" style="text-align:center;font-size:140%"><b>Favorite Websites</b><br>
+
+<a href="https://www.facebook.com/">
+<img class="1" src="f.png" alt="Facebook" style="width:42px;height:42px;border:0"></a>
+
+
+<a href="https://www.twitter.com/">
+<img class="1" src="t.png" alt="Twitter" style="width:42px;height:42px;border:0>
+
+<a href="http://9gag.com/">
+<img class="1"  src="9.png" alt="9Gag" style="width:42px;height:42px;border:0">
+
+</p>
+
+
+
+
+
 </body>
 </html>
+

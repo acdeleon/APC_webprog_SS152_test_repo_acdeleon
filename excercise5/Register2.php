1da66c1ca40593dacb8bf8889ef6f1646a3b7611
BuildTools <unconfigured@null.spigotmc.org>
2016-12-14 20:55:43 +0800

    fix

--- excercise5/Register2.php
+++ excercise5/Register2.php
@@ -1,4 +1,3 @@
-<!DOCTYPE HTML> 
 <?php
 include_once 'dbconfig.php';
 if(isset($_POST['btn-save']))
@@ -15,7 +14,7 @@ if(isset($_POST['btn-save']))
 
  // sql query for inserting data into database
  $sql_query = "INSERT INTO users(Name,Nickname,Email,Home_address,Gender,Phone_number,Comments) VALUES('$Name','$Nickname','$Email','$Home_address','$Gender','$Phone_number','%Comments')";
- // sql query for inserting data into database
+
 
  // sql query execution function
  if(mysqli_query($con,$sql_query))
@@ -37,7 +36,9 @@ if(isset($_POST['btn-save']))
  }
  // sql query execution function
 }
-?> 
+?>
+
+<!DOCTYPE HTML>  
 <html>
 <head>
 <style>
@@ -46,40 +47,45 @@ if(isset($_POST['btn-save']))
 </head>
 <body>  
 <?php include 'links.php';?>
+
+ 
 <?php
 // define variables and set to empty values
+$nameErr = $nicknameErr = $emailErr = $genderErr = $phoneErr = $homeadErr = "";
+$Name = $Nickname = $Email = $Gender = $Phone_number = $Comments = $Home_address = "";
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   if (empty($_POST["Name"])) {
-    $Name = "Name is required";
+    $nameErr = "Name is required";
   } else {
     $Name = test_input($_POST["Name"]);
-    if (!preg_match("/^[a-zA-Z-0-9]*$/",$name)) {
-      $Name = "Only letters,numbers and white space allowed"; 
+    if (!preg_match("/^[a-zA-Z-0-9]*$/",$Nickname)) {
+      $nameErr = "Only letters,numbers and white space allowed"; 
     }
   }
     if (empty($_POST["Nickname"])) {
-    $Nickname = "Nickname is required";
+    $nicknameErr = "Nickname is required";
   } else {
-    $Nickname = test_input($_POST["Nickname"]);
+    $Nickname = test_input($_POST["nickname"]);
     if (!preg_match("/^[a-zA-Z ]*$/",$Nickname)) {
-      $Nickname = "Only letters and white space allowed"; 
+      $nicknameErr = "Only letters and white space allowed"; 
     }
   }
   
   if (empty($_POST["Email"])) {
-    $Email = "Email is required";
+    $emailErr = "Email is required";
   } else {
     $Email = test_input($_POST["Email"]);
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $Email = "Invalid email format"; 
+      $emailErr = "Invalid email format"; 
     }
   }
     if (empty($_POST["Phone_number"])) {
-    $Phone_number = "Phonenumber is required";
+    $phoneErr = "Phonenumber is required";
   } else {
-    $Phone_number = test_input($_POST["phone"]);
-    if (!preg_match("/^([0-9]*)$/",$phone)) {
-      $Phone_number = "Only numbers allowed"; 
+    $Phone_number = test_input($_POST["Phone_number"]);
+    if (!preg_match("/^([0-9]*)$/",$Phone_number)) {
+      $phoneErr = "Only numbers allowed"; 
     }
   }
     
@@ -97,7 +103,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   }
 
   if (empty($_POST["Gender"])) {
-    $Gender = "Gender is required";
+    $genderErr = "Gender is required";
   } else {
     $Gender = test_input($_POST["Gender"]);
   }
@@ -111,29 +117,30 @@ function test_input($data) {
 }
 ?>
 <br><br>
+<center>
 <p><span class="error">* required field.</span></p>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
   Name:<input type="text" name="Name" value="<?php echo $Name;?>">
-  <span class="error">* <?php echo $Name;?></span>
+  <span class="error">* <?php echo $nameErr;?></span>
   <br><br>
   Nickname:<input type="text" name="Nickname" value="<?php echo $Nickname;?>">
-  <span class="error">* <?php echo $Nickname;?></span>
+  <span class="error">* <?php echo $nicknameErr;?></span>
   <br><br>
   E-mail: <input type="text" name="Email" value="<?php echo $Email;?>">
-  <span class="error">* <?php echo $Email;?></span>
+  <span class="error">* <?php echo $emailErr;?></span>
   <br><br>
   Phonenumber: <input type="text" name="Phone_number" value="<?php echo $Phone_number;?>">
-  <span class="error">* <?php echo $Phone_number;?></span>
+  <span class="error">* <?php echo $phoneErr;?></span>
   <br><br>
   Home Address: <input type="text" name="Home_address" value="<?php echo $Home_address;?>">
-  <span class="error"><?php echo $Home_address;?></span>
+  <span class="error"><?php echo $homeadErr;?></span>
   <br><br>
   Comment: <textarea name="Comments" rows="5" cols="40"><?php echo $Comments;?></textarea>
   <br><br>
   Gender:
-  <input type="radio" name="gender" <?php if (isset($Gender) && $Gender=="female") echo "checked";?> value="female">Female
-  <input type="radio" name="gender" <?php if (isset($Gender) && $Gender=="male") echo "checked";?> value="male">Male
-  <span class="error">* <?php echo $Gender;?></span>
+  <input type="radio" name="Gender" <?php if (isset($Gender) && $Gender=="female") echo "checked";?> value="female">Female
+  <input type="radio" name="Gender" <?php if (isset($Gender) && $Gender=="male") echo "checked";?> value="male">Male
+  <span class="error">* <?php echo $genderErr;?></span>
   <br><br>
   <input type="submit" name="btn-save" value="Submit"></center> 
 </form>

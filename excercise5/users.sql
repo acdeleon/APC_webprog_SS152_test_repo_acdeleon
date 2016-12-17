693f1a8f2db11c44fe19abd2b895c7d880ff76bd
BuildTools <unconfigured@null.spigotmc.org>
2016-12-17 22:13:50 +0800

    updated users.sql

--- excercise5/users.sql
+++ excercise5/users.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 12, 2016 at 03:19 PM
+-- Generation Time: Dec 17, 2016 at 03:13 PM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 5.6.23
 
@@ -31,19 +31,11 @@ CREATE TABLE `users` (
   `Name` varchar(25) NOT NULL,
   `Nickname` varchar(25) NOT NULL,
   `Email` varchar(25) NOT NULL,
-  `Phone_number` int(25) NOT NULL,
+  `Phone_number` varchar(25) NOT NULL,
   `Home_address` varchar(25) NOT NULL,
   `Gender` varchar(25) NOT NULL,
   `Comments` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`user_id`, `Name`, `Nickname`, `Email`, `Phone_number`, `Home_address`, `Gender`, `Comments`) VALUES
-(1, 'qwe', 'qwe', 'qwe', 123123, 'qwe', '', ''),
-(2, 'dsf', 'sdf', 'sdf', 0, 'sdfsdf', '', '');
 
 --
 -- Indexes for dumped tables
@@ -63,7 +55,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

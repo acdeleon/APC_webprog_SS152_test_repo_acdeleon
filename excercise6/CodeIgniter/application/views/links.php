e7be13df675da9fe0027cf354816b93f87b58922
BuildTools <unconfigured@null.spigotmc.org>
2016-12-21 18:43:08 +0800

    exercise 6 done

--- excercise6/CodeIgniter/application/views/links.php
+++ excercise6/CodeIgniter/application/views/links.php
@@ -1,5 +1,5 @@
 <ul>
 <li><a href="<?php echo site_url('Home');?>">Home</a></li>
 <li><a href="<?php echo site_url('More');?>">More about me</a></li>
-<li><a href="<?php echo site_url('Register');?>">Contact me</a></li>
+<li><a href="<?php echo site_url('users');?>">Contact me</a></li>
 </ul>
